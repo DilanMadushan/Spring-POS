659dcb8949691223fe9e54bda44f27443b0595d7
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-10-14 20:08:59 +0530

    add cross origin anotation

--- src/main/java/lk/ijse/springpos/controller/OrderController.java
+++ src/main/java/lk/ijse/springpos/controller/OrderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/order")
 public class OrderController{
--- src/main/java/lk/ijse/springpos/controller/UserController.java
+++ src/main/java/lk/ijse/springpos/controller/UserController.java
@@ -4,11 +4,9 @@ import jakarta.annotation.PostConstruct;
 import lk.ijse.springpos.dto.UserDTO;
 import lk.ijse.springpos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/user")
 public class UserController {
